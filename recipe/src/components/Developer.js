ab3d5596b6f98ac79687a10879197e51de0059bb
Yifei Zhou <yfzhou23@gmail.com>
2024-03-02 21:46:47 +0000

    Edit Developer Page

--- recipe/src/components/Developer.js
+++ recipe/src/components/Developer.js
@@ -9,10 +9,10 @@ const Developer = () => {
             <h1>Developer</h1>
             <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', overflow: 'hidden' }}>
                 <div key={index} style={{ margin: '20px', padding: '20px', border: '1px solid black', width: 'calc(50% - 40px)' }}>
-                    <img src="" alt={member.name} style={{ width: '100px', height: '100px' }} />
-                    <h2 style={{ whiteSpace: 'nowrap' }}>{member.name}</h2>
-                    <h3 style={{ whiteSpace: 'nowrap' }}>{member.role}</h3>
-                    <p>{member.bio}</p>
+                    <img src="ProfilePhoto.jpeg" alt="Yifei Zhou" style={{ width: '100px', height: '100px' }} />
+                    <h2 style={{ whiteSpace: 'nowrap' }}>Yifei Zhou</h2>
+                    <h3 style={{ whiteSpace: 'nowrap' }}>Developer of the Website</h3>
+                    <p>MS Computer Science Student at Brandeis University</p>
                 </div>
             </div>
         </div>
