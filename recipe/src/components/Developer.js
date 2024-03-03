df3aad4138a0640aede4935c997e1590043c82c2
Yifei Zhou <yfzhou23@gmail.com>
2024-03-03 01:11:07 +0000

    Add map images

--- recipe/src/components/Developer.js
+++ recipe/src/components/Developer.js
@@ -1,19 +1,119 @@
 import React from 'react';
+import Container from 'react-bootstrap/esm/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
+import { styles } from '../styles';
+import { navLinks } from '../constants';
+import {ProfilePhoto, bwmap, worldmap } from '../assets';
+
 
 const Developer = () => {
 
     return (
-        <div>
-            <h1>Developer</h1>
-            <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', overflow: 'hidden' }}>
-                <div key={index} style={{ margin: '20px', padding: '20px', border: '1px solid black', width: 'calc(50% - 40px)' }}>
-                    <img src="ProfilePhoto.jpeg" alt="Yifei Zhou" style={{ width: '100px', height: '100px' }} />
-                    <h2 style={{ whiteSpace: 'nowrap' }}>Yifei Zhou</h2>
-                    <h3 style={{ whiteSpace: 'nowrap' }}>Developer of the Website</h3>
-                    <p>MS Computer Science Student at Brandeis University</p>
-                </div>
-            </div>
+        // <div className='main-content'>
+        //     <Card style={{ width: '36rem', height: '50rem', margin: '10px', backgroundColor: 'white', border: '1px solid white' }}>
+        //         <div className="d-flex justify-content-center align-items-center">
+        //           <Card.Img variant="top" src="ProfilePhoto.JPG" alt="Yifei Zhou" style={{ width: '34rem', height: '36rem', marginBottom: '10px' }} />
+        //         </div>
+        //         <Card.Body>
+        //           <Card.Title style={{ fontSize: '1.2rem', marginBottom: '10px' }}>Developer: Yifei Zhou</Card.Title>
+        //           <Card.Text>
+        //             MS Computer Science Student at Brandeis University
+        //           </Card.Text>
+        //         </Card.Body>
+        //       </Card>
+        // </div>
+
+        <>
+      <div className="absolute top-0 left-0 z-0 h-[100vh] w-screen">
+        <img
+          src={bwmap}
+          alt="world map"
+          className="w-full h-full sm:block hidden object-cover"
+        />
+      </div>
+      <div className="absolute top-0 left-0 z-0 h-[100vh] w-screen">
+        <img
+          src={worldmap}
+          alt="world map"
+          className="w-full h-full sm:hidden block object-cover"
+        />
+      </div>
+      <section
+        className="relative flex sm:flex-row flex-col w-full h-screen mx-auto 
+        sm:bg-hero bg-hero-mobile overflow-hidden">
+        <div
+          className={`absolute inset-0 sm:top-[250px] top-[150px] 
+          lg:top-[150px] xl:top-[250px] ${styles.paddingX} 
+          max-w-7xl mx-auto flex flex-row items-start
+          justify-between gap-3`}>
+          <div className="flex flex-col justify-center items-center mt-5 ml-3">
+            <div className="w-5 h-5 rounded-full bg-[#0a0a0a] sm:hidden" />
+            <div className="w-1 sm:h-80 h-40 bw-gradient sm:hidden" />
+          </div>
+
+          <div>
+            <h1
+              className={`${styles.heroHeadText} text-eerieBlack font-poppins uppercase`}>
+              Hi, I'm{' '}
+              <span
+                className="sm:text-battleGray sm:text-[90px] 
+                text-eerieBlack text-[50px] font-mova
+                font-extrabold uppercase">
+                Shaquille
+              </span>
+            </h1>
+            <p className={`${styles.heroSubText} mt-2 text-eerieBlack`}>
+              Lorem ipsum dolor sit amet. <br className="sm:block hidden" />
+              consectetur adipisicing elit deleniti, voluptas.
+            </p>
+          </div>
+          <div
+            className="w-screen flex flex-col items-start 
+            justify-center sm:-ml-[3rem] xxs:mt-4"></div>
+
+          <div></div>
         </div>
+
+        <div
+          className="absolute xs:bottom-10 bottom-32 w-full 
+          flex justify-center items-center">
+          <a href="#about">
+            <div
+              className="w-[35px] h-[64px] rounded-3xl border-4 
+            border-french border-dim flex
+            justify-center items-start p-2">
+              <motion.div
+                animate={{
+                  y: [0, 24, 0],
+                }}
+                transition={{
+                  duration: 1.5,
+                  repeat: Infinity,
+                  repeatType: 'loop',
+                }}
+                className="w-3 h-3 rounded-full bg-taupe mb-1"
+              />
+            </div>
+          </a>
+        </div>
+
+        {/* Your image comes here. Feel free to remove image if you don't plan to have one.*/}
+        <div>
+          <img
+            className="absolute bottom-0 ml-[50vw] 
+            lg:ml-[75vw] md:ml-[60vw] xmd:ml-[60vw] 2xl:ml-[83vw]
+            sm:h-[90vh] md:h-[70vh] xl:h-[80vh]"
+            src={shaq}
+            alt="shaquille"
+          />
+        </div>
+      </section>
+    </>
+
     );
 }
 
