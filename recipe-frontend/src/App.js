47c365a5d447be1e3d0d1aaa5949316dbc84946b
Yifei Zhou <yfzhou23@gmail.com>
2024-03-14 03:51:32 +0000

    Change Titile

--- recipe-frontend/src/App.js
+++ recipe-frontend/src/App.js
@@ -71,7 +71,7 @@ const App = () => {
     <Router>
       <div className="App">
         <header className="App-header">
-          <h1>My Recipe Book</h1>
+          <h1>Chinese Recipes Book</h1>
         </header>
         <Navbar expand="lg" className="bg-body-tertiary">
           <Container>
