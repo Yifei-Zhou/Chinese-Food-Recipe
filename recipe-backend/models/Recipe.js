c60df9e0b044b258e2e126f925148473a5cbdb0f
Yifei-Zhou <yfzhou23@gmail.com>
2024-04-27 02:48:17 -0400

    Update Recipe Schema

--- recipe-backend/models/Recipe.js
+++ recipe-backend/models/Recipe.js
@@ -8,7 +8,7 @@ const recipeSchema = new mongoose.Schema({
   ingredients: [String],
   ingredientsInfo: [String],
   instructions: [String],
-  image: String,
+  image: [String],
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
