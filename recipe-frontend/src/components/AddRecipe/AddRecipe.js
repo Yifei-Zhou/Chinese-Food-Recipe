cb2b1b44c14c3a2d26d219d2b86a08ca8f7318f3
Yifei-Zhou <yfzhou23@gmail.com>
2024-04-29 02:05:44 -0400

    Import Button in AddRecipe

--- recipe-frontend/src/components/AddRecipe/AddRecipe.js
+++ recipe-frontend/src/components/AddRecipe/AddRecipe.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
-
+import Button from 'react-bootstrap/Button';
 
 const AddRecipe = () => {
   const [title, setTitle] = useState('');
